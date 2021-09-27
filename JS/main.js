49048aa5243a2f892ad72393d2aeaf913950f9b6
Diego Roza <diego.roza@edu-kw1c.nl>
2021-09-27 15:00:41 +0200

    wrong repo whoops

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,53 @@
-// Main file
+// Set Add Card Event
+$('.productCard').on("click", function (event) {
+    let target = $(event.target);
+    let parent = target.parent();
+
+    const html = `<div class="card" style="background-color:${$getRandomColor()};"><p>New text</p></div>`
+    return parent.append(html);
+});
+
+function $getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Document ready
+$(function () {
+    // Declare API
+    window.$API = new $InitialiseAPI();
+    // Declare Cards
+    window.$cards = {};
+    // Intialise API call -- Check for content
+    $API.Get();
+});
+class $InitialiseAPI {
+    Post = () => {
+        $.post("PYTHON URL", function (data) {
+                // Succes
+            })
+            .done(function (data) {
+                // After complete
+            })
+            .fail(function (error) {
+                // If failed
+            });
+    };
+
+    Get = () => {
+        $.get("PYTHON URL", function (data) {
+                // Succes
+            })
+            .done(function (data) {
+                // After complete
+
+            })
+            .fail(function (error) {
+                // If failed
+            });
+    };
+};
