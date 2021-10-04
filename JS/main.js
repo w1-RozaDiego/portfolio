f1d068fcad63bda06179041e452fb0996e54793a
Diego Roza <diego.roza@edu-kw1c.nl>
2021-10-04 15:06:37 +0200

    .

--- JS/main.js
+++ JS/main.js
@@ -2,49 +2,69 @@
 $('.productCardAdd').on("click", function (event) {
     let target = $(event.target);
     let parent = target.parent().parent();
+    const html = `
+    <div class="productCard card col-4" style="background-color: ${$getRandomColor()};">
+        <!-- Content -->
+    </div>`;
+    parent.append(html);
+    
+    $('#plPopupModal').data('card', html);
     $('#plPopupModal').modal('toggle');
-    $('#plPopupModal').data('card', target.parent());
     $('#plPopupModal #plModalTitle').text('Aanmaken card');
     $('#plPopupModal .modal-body').html(`
         <label for="Name">Naam:</label><br/>
-        <input type="text" id="plPopupName" name="Name"><br/>
+        <input type="text" id="plPopupName" name="Name" class="w-100"><br/>
         <label for="Pomp">Pomp:</label><br/>
-        <input type="text" id="plPopupPomp" name="Pomp"><br/>
+        <input type="text" id="plPopupPomp" name="Pomp" class="w-100"><br/>
         <label for="Duration">Duration:</label><br/>
-        <input type="text" id="plPopupDuration" name="Duration"><br/>
+        <input type="text" id="plPopupDuration" name="Duration" class="w-100"><br/>
         <label for="Repeat">Herhaling:</label><br/>
-        <input type="text" id="plPopupRepeat" name="Repeat"><br/>
+        <input type="text" id="plPopupRepeat" name="Repeat" class="w-100"><br/>
         <label for="Description">Omschrijving:</label><br/>
-        <input type="textbox" id="plPopupDescription" name="Description">
-    `); 
-
-    const html = `
-                    <div class="productCard card col-4" style="background-color: ${$getRandomColor()};">
-                        <!-- Content -->
-                        <h5 class="card-title">Naam: <span id="Name"> </span> </h5>
-                    </div>`;
-    var newObj = parent.append(html);
-    $setPopup();
+        <input type="text" id="plPopupDescription" name="Description" class="w-100">
+    `);
 });
 
 // Dirty way to close the modal
-$('.closeModal').each(function(){
-    $(this).on("click",function(){
+$('.closeModal').each(function () {
+    $(this).on("click", function () {
         $('#plPopupModal').modal('toggle');
     });
 });
 // Dirty way to save the modal and close it
-$('#saveModal').each(function(){
-    $(this).on("click",function(){
+$('#saveModal').each(function () {
+    $(this).on("click", function () {
         // Save data from modal and set on the card
         var card = $('#plPopupModal').data('card');
         val = [];
-        $('#plPopupModal .modal-body').children().each(function(){
-            if($(this).is("input[type=text]")){
+        $('#plPopupModal .modal-body').children().each(function () {
+            if ($(this).is("input[type=text]")) {
                 val.push(this.value);
             }
         });
-        console.log(val)
+        var name = val[0],
+            pomp = val[1],
+            duration = val[2],
+            repeat = val[3],
+            description = val[4];
+
+        card.html(`
+            <div class="container-fluid" style="text-align:center;">
+                <h3 style="text-align:center;>${name}</h3>
+            </div>
+        `);
+        card.on("click", function () {
+            $(this).html(`
+            <div class="container-fluid" style="text-align:center;">
+                <h3 style="text-align:center;>${name}</h3><br/>
+                <p>${pomp}</p><br/>
+                <p>${duration}</p><br/>
+                <p>${repeat}</p><br/>
+                <p>${description}</p><br/>
+            </div>
+            `);
+        });
+
         $('#plPopupModal').modal('toggle');
         // Save shit
     });
@@ -76,7 +96,7 @@ $getRandomColor = () => {
 // Create random color each 3 seconds
 $dynamicallyChangeColors = () => {
     async function setColor() {
-        $('.productCard').each(function(){
+        $('.productCard').each(function () {
             $(this).css({
                 'background-color': $getRandomColor
             });
